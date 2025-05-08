7ceab951c9a5a318eded31be37b99278dc3b1c83
ProObito <ahmedumaid707@gmail.com>
2025-05-08 12:58:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "20718334")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8065030679:AAE-Vvvejh6maceHLBamYQPOYIyC-sMUM2w") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8065030679:AAFGbpVsYJC9TQ4b7TdKE9QfsECUJtkl2cs") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002200709110') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
@@ -17,14 +17,14 @@ class Config(object):
     DB_NAME  = os.environ.get("DB_NAME","Cluster0") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "5585016974")) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", "5585016974 7328629001")) # ⚠️ Required
     LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '--1002200709110')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://envs.sh/qDs.jpg")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "8080"))
+    PORT = int(os.environ.get("PORT", "5572"))
 
 
     caption = """
